968c089c5e65e5300d407c37a6011ad7065a155c
Matt Moore <matt@mattmoore.io>
2024-02-13 00:56:32 -0500

    Initial commit

---
+++ water-cli/src/main.rs
@@ -0,0 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    game: String,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    println!("{}!", args.game)
+}
