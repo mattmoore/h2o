609a9d3674dc6dfe23ce12f6b99697a698ff3ebb
Matt Moore <matt@mattmoore.io>
2024-02-18 09:49:12 -0500

    Add game catalog

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -4,8 +4,10 @@ use libh2o::*;
 
 #[derive(Subcommand, Debug, Clone)]
 enum Commands {
-    #[command(about = "Download a game.")]
-    Download { game: String },
+    #[command(about = "List available games.")]
+    List,
+    #[command(about = "Install a game.")]
+    Install { game: String },
     #[command(about = "Run a game.")]
     Run { game: String },
 }
@@ -20,11 +22,14 @@ struct Args {
 fn main() {
     let cmd = Args::parse();
     match &cmd.action {
-        Commands::Download { game } => {
-            unpack_game(&game.to_string()).expect("Failed to download {game}.")
+        Commands::List => {
+            list().expect("Failed to list catalog.")
+        }
+        Commands::Install { game } => {
+            install(game.to_string()).expect("Failed to install {game}.")
         }
         Commands::Run { game } => {
-            run_game(&game.to_string()).expect("Failed to run {game}.")
+            run(game.to_string()).expect("Failed to run {game}.")
         }
     }
 }
--- libh2o/src/lib.rs
+++ libh2o/src/lib.rs
@@ -1,35 +1,83 @@
 use dirs::home_dir;
+use std::collections::HashMap;
 use std::fs::File;
-use std::io;
+use std::io::Result;
+use std::path::PathBuf;
 use std::process::Command;
 use tar::Archive;
 use xz::read::XzDecoder;
 
-pub fn unpack_game(game: &str) -> io::Result<()> {
-    if let Some(home) = home_dir() {
-        let h2o_dir = &home.join(".h2o");
-        let source = h2o_dir.join("downloads").join(format!("{game}-linux-x86_64.tar.xz"));
-        let target = h2o_dir.join("games");
+#[derive(Clone)]
+pub struct CatalogItem {
+    pub name: String,
+    pub target: String,
+    pub download_target: String,
+}
 
-        println!("Installing {game}.");
+pub fn catalog() -> Result<HashMap<String, CatalogItem>> {
+    let mut catalog: HashMap<String, CatalogItem> = HashMap::new();
+    catalog.insert(
+        String::from("fantasy"),
+        CatalogItem {
+            name: String::from("Fantasy"),
+            target: String::from("Fantasy/Fantasy.sh"),
+            download_target: String::from("fantasy-linux-x86_64.tar.xz"),
+        },
+    );
+    catalog.insert(
+        String::from("test"),
+        CatalogItem {
+            name: String::from("Fantasy"),
+            target: String::from("Fantasy/Fantasy.sh"),
+            download_target: String::from(""),
+        },
+    );
+    Ok(catalog)
+}
 
-        let tar_xz = File::open(source)?;
-        let tar = XzDecoder::new(tar_xz);
-        let _ = Archive::new(tar).unpack(target);
-
-        println!("Finished installing {game}.");
+pub fn list() -> Result<()> {
+    for item in catalog()?.keys() {
+        println!("{item}")
     }
 
     Ok(())
 }
 
-pub fn run_game(game: &str) -> io::Result<()> {
+pub fn install(game: String) -> Result<()> {
     if let Some(home) = home_dir() {
         let h2o_dir = &home.join(".h2o");
-        let target = h2o_dir
-            .join("games")
-            .join(game)
-            .join("fantasy.sh");
+        let _ = unpack(h2o_dir.to_path_buf(), game);
+    }
+
+    Ok(())
+}
+
+pub fn unpack(h2o_dir: PathBuf, game: String) -> Result<()> {
+    let source = h2o_dir
+        .join("downloads")
+        .join(format!("{game}-linux-x86_64.tar.xz"));
+    let target = h2o_dir.join("games");
+
+    println!("Installing {game}.");
+
+    let tar_xz = File::open(source)?;
+    let tar = XzDecoder::new(tar_xz);
+    let _ = Archive::new(tar).unpack(target);
+
+    println!("Finished installing {game}.");
+
+    Ok(())
+}
+
+pub fn run(game: String) -> Result<()> {
+    if let Some(home) = home_dir() {
+        let h2o_dir = &home.join(".h2o");
+        let game_target = catalog()?
+            .get(&game)
+            .expect("Catalog item doesn't exist")
+            .to_owned()
+            .target;
+        let target = h2o_dir.join("games").join(game_target);
 
         println!("target: {}", target.display());
         println!("Running {game}...");
